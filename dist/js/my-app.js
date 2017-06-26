0e61d1234601082c55fec7e07467d23bfcf5362a
周刚 <zhougang@zhougangdeMacBook-Air.local>
2017-06-26 11:38:49 +0800

    register

--- dist/js/my-app.js
+++ dist/js/my-app.js
@@ -1,7 +1,8 @@
 const dataUrl = "http://192.168.31.107:8080"
 // Initialize your app
 var myApp = new Framework7({
-    cache: true
+    cache: true,
+    precompileTemplates: true
 });
 
 // Export selectors engine
@@ -56,7 +57,6 @@ myApp.onPageInit('message', function (page) {
             dataType:"json",
             data: {"content":val},
             success: function(data){
-                console.log(data.status);
                 mainView.router.loadPage("chat.html");
             },
             error: function(data){
@@ -66,8 +66,7 @@ myApp.onPageInit('message', function (page) {
     });
 });
 
-
-myApp.onPageInit('chat', function (page) {
+myApp.onPageAfterAnimation('chat', function (page) {
     $$.get(dataUrl + '/msg?skip=0',function(data){
             //console.log(data)
             var obj = eval('(' + data + ')');
@@ -75,13 +74,12 @@ myApp.onPageInit('chat', function (page) {
             var content = "";
             $$.each(obj.data,function(key,val){
             content += '<div class="img-block">' +
-                            '<div class="msg-user">匿名用户 ' + val.created_at + '</div>' +
-                            '<div>' + val.content +'</div>' +
+                            '<div class="msg-user">匿名用户 ' + val.Created_at + '</div>' +
+                            '<div>' + val.Content +'</div>' +
                         '</div>';
             });
             $$("#msg-list").append(content);
         });
-
     //无限滚动
     var loading = false;
     $$('.infinite-scroll').on('infinite', function () {
@@ -108,14 +106,14 @@ myApp.onPageInit('chat', function (page) {
                 }
                 $$.each(arr, function(key,val){
                 content += '<div class="img-block">' +
-                                '<div class="msg-user">匿名用户 ' + val.created_at + '</div>' +
-                                '<div>' + val.content +'</div>' +
+                                '<div class="msg-user">匿名用户 ' + val.Created_at + '</div>' +
+                                '<div>' + val.Content +'</div>' +
                             '</div>';
                 });
                 $$("#msg-list").append(content);
             });
-        loading = false;
-    });  
+        loading = false; 
+    });
 });
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
@@ -129,6 +127,156 @@ myApp.onPageInit('about', function (page) {
 
         });
 
+});
+//template
+var hasLoginHTML = Template7.templates.hasLoginTemplate({
+                    'header': dataUrl + localStorage.getItem('userHeader'),
+                    'name': localStorage.getItem('userName'),
+                });
+var notLoginHTML = Template7.templates.notLoginTemplate();
+//if login
+myApp.onPageInit('user', function (page) {
+    createLoginPage();
+    $$("#btn-login").click(function(){
+        var phone = $$("#login-form input[name=phone]").val();
+        var password = $$("#login-form input[name=password]").val();
+        if (phone.length == 0) {
+            myApp.alert('手机号不能为空;','错误');
+            return;
+        }
+        if (password.length == 0) {
+            myApp.alert('密码不能为空;','错误');
+            return;
+        }
+        //ajax post login
+        myApp.showIndicator();
+        $$.ajax({
+            method:"post",
+            url: dataUrl + "/login",
+            dataType:"json",
+            data: {"phone":phone,"password":password},
+            success: function(data){
+                myApp.hideIndicator();
+                if (data.code != 200){
+                    myApp.alert(data.msg,'错误');
+                    return;
+                }
+                localStorage.setItem('login', 1);
+                localStorage.setItem('token', data.token);
+                localStorage.setItem('userName', data.userName);
+                localStorage.setItem('userHeader', data.userHeader);
+                localStorage.setItem('userId', data.userId);
+                hasLoginHTML = Template7.templates.hasLoginTemplate({
+                    'header': dataUrl + data.userHeader,
+                    'name': data.userName,
+                });
+                createLoginPage();
+            },
+            error: function(data){
+                myApp.hideIndicator();
+                myApp.alert(data.textStatus,'错误');
+            }
+        });
+
+    });
+    $$("#btn-logout").click(function(){
+        localStorage.removeItem('login');
+        localStorage.setItem('token', '');
+        createLoginPage();
+    });
+});
+
+function createLoginPage(){
+    if (localStorage.getItem('login')) {
+        $$("#btn-logout").show();
+        $$("#btn-login").hide();
+        $$("#forget-psd").hide();
+        $$("#login-page").html(hasLoginHTML);
+        changeNameEvent();
+    } else {
+        $$("#btn-login").show();
+        $$("#btn-logout").hide();
+        $$("#forget-psd").show();
+        $$("#login-page").html(notLoginHTML);
+    }
+}
+
+function changeNameEvent() {
+    $$('#change-name').on('click', function () {
+        myApp.prompt('','修改昵称', function (value) {
+            if(value.length == 0 || value.replace(/\ +/g,"") == "") {
+                myApp.alert("昵称不能为空","错误");
+                return 
+            }
+            $$.ajax({
+                method:"get",
+                url: dataUrl + "/v1/change-name",
+                dataType:"json",
+                headers: {'Authorization':'Bearer ' + localStorage.getItem('token')},
+                data: {"name": value,"userId":localStorage.getItem('userId')},
+                success: function(data){
+                    if (data.code != 200) {
+                        myApp.alert(data.msg,'错误');
+                        return
+                    }
+                    localStorage.setItem('userName', data.userName);
+                    $$('#change-name').text(data.userName);
+                },
+                error: function(data){
+                    myApp.alert("服务器错误",'错误');
+                }
+           });
+        });
+    });
+}
+//register html
+myApp.onPageInit('register', function (page) {
+    $$("#btn-register").click(function(){
+        var phone = $$("#register-form input[name=phone]").val();
+        var password = $$("#register-form input[name=password]").val();
+        var password2 = $$("#register-form input[name=password2]").val();
+        if (phone.length == 0) {
+            myApp.alert('手机号不能为空;','错误');
+            return;
+        }
+        if (password.length == 0) {
+            myApp.alert('密码不能为空;','错误');
+            return;
+        }
+        if (password2.length == 0) {
+            myApp.alert('密码确认不能为空;','错误');
+            return;
+        }
+        //ajax post login
+        myApp.showIndicator();
+        $$.ajax({
+            method:"post",
+            url: dataUrl + "/register",
+            dataType:"json",
+            data: {"phone":phone,"password":password,"password2":password2},
+            success: function(data){
+                myApp.hideIndicator();
+                if (data.code != 200){
+                    myApp.alert(data.msg,'错误');
+                    return;
+                }
+                localStorage.setItem('login', 1);
+                localStorage.setItem('token', data.token);
+                localStorage.setItem('userName', data.userName);
+                localStorage.setItem('userHeader', data.userHeader);
+                localStorage.setItem('userId', data.userId);
+                hasLoginHTML = Template7.templates.hasLoginTemplate({
+                    'header': dataUrl + data.userHeader,
+                    'name': data.userName,
+                });
+                mainView.router.loadPage('user.html');
+            },
+            error: function(data){
+                myApp.hideIndicator();
+                myApp.alert(data.textStatus,'错误');
+            }
+        });
+    });
 });
 
 // Generate dynamic page
